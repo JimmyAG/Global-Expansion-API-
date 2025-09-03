44a79ff38e2291b9217c00d53494e8201a57467a
Thoth <jimmymoham10@gmail.com>
2025-09-03 09:17:29 +0300

    update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,6 +32,7 @@ export default tseslint.config(
       '@typescript-eslint/nounsafe-call': 'off',
       '@typescript-eslint/nounsafe-return': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/require-await': 'warn',
     },
