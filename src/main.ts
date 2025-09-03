a14c36e6959ea9fe8240c419cbb8d6dfa4f0aa19
Thoth <jimmymoham10@gmail.com>
2025-09-03 09:06:28 +0300

    updated main.ts bootstrap function

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('API for authentication system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Docs avaiable at: /api
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap();
