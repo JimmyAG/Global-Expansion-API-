99138686dd401224513d033f46c969d6c3afe32b
Thoth <jimmymoham10@gmail.com>
2025-09-06 17:01:11 +0300

    created a bash script to trigger seeding on nest app container start

---
+++ docker-db-prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+echo "Waiting for MongoDB..."
+until nc -z expanders360_mongodb 27017; do
+  sleep 1
+done
+
+echo "Waiting for MySQL..."
+until nc -z expanders360_mysql 3306; do
+  sleep 1
+done
+
+echo "Waiting for Redis..."
+until nc -z redis 6379; do
+  sleep 1
+done
+
+echo "Running seed script..."
+npm run seed -- -v 200 -c 500 -s 
+
+echo "Starting NestJS app..."
+exec npm run start:dev 
